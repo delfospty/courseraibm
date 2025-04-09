1eadf3c20df08b24c5ea37a4cec3f7ad0fc7238c
Alex <alex@airesistemas.com>
2025-04-09 18:18:46 -0500

    Ultimos cambios en var.

--- scope/scope_lab.js
+++ scope/scope_lab.js
@@ -8,8 +8,8 @@ var blockVar = "I'm a block-scoped var";
 let blockLet = "I'm a block-scoped let";
 const blockConst = "I'm a block-scoped const";
    //Block Scope
-console.log(blockVar);
-console.log(blockLet);
+//console.log(blockVar);
+//console.log(blockLet);
    
 }
    // Global scope
@@ -17,4 +17,31 @@ console.log(blockLet);
 //   console.log(globalLet); // Output: "I'm also global, but scoped with let"
 //   console.log(globalConst); // Output: "I'm a global constant"
 
-   
+
+function show(){
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+    }
+    show();
+//    console.log(functionVar); // Throws ReferenceError
+//    console.log(functionLet); // Throws ReferenceError
+//    console.log(functionConst); // Throws ReferenceErro
+  
+{
+var changeVar = "I'm a block-scoped var";
+let changeLet = "I'm a block-scoped let";
+const changeConst = "I'm a block-scoped const";
+
+console.log(changeVar); // Output: "I'm a block-scoped var"
+console.log(changeLet); // Output: "I'm a block-scoped let"
+console.log(changeConst); // Output: "I'm a block-scoped const"
+
+changeVar = "I can be changed"; // Reassigning var
+changeLet = "I can also be changed"; // Reassigning let
+// changeConst = "I cannot be changed"; // Throws TypeError: Assignment to constant variable
+
+}
+
+changeVar = "I can be changed outside the block"; // Reassigning var
+console.log(changeVar); // Output: "I can be changed outside the block"
